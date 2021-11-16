a2ee4cdfd6bac1b3dc3de341ec02309f56bfbed8
Jhon Ayrton Saavedra Castañeda <ayrtonsc6@gmail.com>
2021-11-16 14:25:25 -0500

    📗 feat: add unique rule to project user

--- src/app/Http/Requests/ProjectUser/CreateRequest.php
+++ src/app/Http/Requests/ProjectUser/CreateRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests\ProjectUser;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\Rule;
 
 class CreateRequest extends FormRequest
 {
@@ -32,8 +34,18 @@ class CreateRequest extends FormRequest
             'user_id' => [
                 'required',
                 'integer',
-                'exists:users,id'
+                'exists:users,id',
+                Rule::unique('project_users','user_id')->where(function($query){
+                    return $query->where('project_id', $this->input('project_id'));
+                })
             ]
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'user_id.unique' => 'This user have already been registered in this project.'
         ];
     }
 }
